08544c1f735055050a408bae13c39953d2a2b961
Alexcalyx <52861884+Alexcalyx@users.noreply.github.com>
2023-07-25 07:34:20 +0900

    Update subject.js

--- subject.js
+++ subject.js
@@ -1,9 +1,1 @@
-// Subject class - Represents a subject with a name
-class Subject {
-    constructor(name, type) {
-      this.name = name;
-    }
-  }
-  
-  // Export the Subject class
-  export default Subject;
+
